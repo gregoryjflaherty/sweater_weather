d837d8725d76318d718744c4d137d75ffb30224c
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-04-26 13:08:20 -0400

    Feat: add create route for users

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :forecast, only: :index
       resources :backgrounds, only: :index
       resources :munchies, only: :index
+      resources :users, only: :create
     end
   end
 end
