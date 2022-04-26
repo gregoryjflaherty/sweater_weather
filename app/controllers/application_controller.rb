d4fb77b95406d935f16f64ea820342e8b93c51b0
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-04-26 13:07:10 -0400

    Feat: refactor error in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
   def bad_request
-    render json: {message: 'Request Invalid: Please check paramters and try again'}, status: :bad_request
-
+    render json: {message: 'Request Invalid: Please check paramters and try again'}, status: 400
   end
 end
