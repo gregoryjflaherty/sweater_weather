d402963553db5ae6891a78bbcf948fe68ce18f13
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-04-27 11:56:06 -0400

    Feat: add uvi and take out min temp

--- app/poros/hourly.rb
+++ app/poros/hourly.rb
@@ -1,14 +1,12 @@
 class Hourly
   attr_reader :time,
               :temperature,
-              :min_temp,
               :conditions,
               :icon
 
   def initialize(data)
     @time = Time.at(data[:dt]).strftime("%H:%M:%S")
     @temperature = data[:temp]
-    @min_temp = data[:temp]
     @conditions = data[:weather][0][:description]
     @icon = data[:weather][0][:icon]
   end
