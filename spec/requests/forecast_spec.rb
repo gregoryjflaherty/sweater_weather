39f97cd54c83e12e0f21393babc519393e42bbfe
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-04-27 09:18:33 -0400

    Test: create test for forecast request

--- spec/requests/forecast_spec.rb
+++ spec/requests/forecast_spec.rb
@@ -14,4 +14,38 @@ RSpec.describe 'Search Forecast' do
       expect(expected[:data][:attributes].keys).to include(:hourly_weather)
     end
   end
+
+  context 'Sad Path' do
+    scenario 'returns error if paramters bad or incomplete', :vcr do
+
+      params = {location: ""}
+      get api_v1_forecast_index_path, params: params
+
+      expected = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      expect(expected.keys.length).to eq(1)
+      expect(expected.keys[0]).to eq(:message)
+      expect(expected.keys[0]).to_not eq(:data)
+
+      expect(expected[:message]).to eq("Request Invalid: Please check paramters and try again")
+    end
+
+    scenario 'returns error if paramters bad or incomplete', :vcr do
+
+      params = {location: 456}
+      get api_v1_forecast_index_path, params: params
+
+      expected = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      expect(expected.keys.length).to eq(1)
+      expect(expected.keys[0]).to eq(:message)
+      expect(expected.keys[0]).to_not eq(:data)
+
+      expect(expected[:message]).to eq("Request Invalid: Please check paramters and try again")
+    end
+  end
 end
