b2d1308b5b956889af614c04748fb4edea929f2d
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-07-25 19:05:52 -0400

    Test: test relationships for trip model

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it { should have_many(:trip_users)}
+    it { should have_many(:users).through(:trip_users)}
 end
