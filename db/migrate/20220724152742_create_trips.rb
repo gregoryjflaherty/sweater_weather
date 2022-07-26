3e172aa68997c409ec285c43bbcca9c930a25a40
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-07-26 09:12:53 -0400

    Feat: add travel_time to trips table

--- db/migrate/20220724152742_create_trips.rb
+++ db/migrate/20220724152742_create_trips.rb
@@ -3,6 +3,7 @@ class CreateTrips < ActiveRecord::Migration[5.2]
     create_table :trips do |t|
       t.string :start_loc
       t.string :end_loc
+      t.string :travel_time
       t.float :rating
 
       t.timestamps
