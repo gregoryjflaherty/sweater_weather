1eab67c6908aff5aec682e395959a0d504f4465f
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-07-27 09:46:25 -0400

    Feat: add authorization to application conrtoller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::API
+
   def bad_request
     render json: {message: 'Request Invalid: Please check paramters and try again'}, status: 400
+  end
+
+    def current_user
+    if session[:current_user]
+      @current_user = User.find(session[:user_id])
+    end
+  end
+
+  def authorize
+    if !current_user
+      render json: {message: "Unauthorized: Please log in first"}, status: 401
+    end
   end
 end
