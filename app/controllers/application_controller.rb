38e7fa22b1c9bd8b1139858ce6bda8fb8b3853fd
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-07-27 13:01:09 -0400

    Refactor: remove current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,10 @@
 class ApplicationController < ActionController::API
+  acts_as_token_authentication_handler_for User, fallback: :none
 
   def bad_request
     render json: {message: 'Request Invalid: Please check paramters and try again'}, status: 400
   end
 
-    def current_user
-    if session[:current_user]
-      @current_user = User.find(session[:user_id])
-    end
-  end
 
   def authorize
     if !current_user
