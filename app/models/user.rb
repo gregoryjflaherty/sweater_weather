e3b12d933e2167e1a196ef2731591ee0829a6997
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-07-27 13:03:05 -0400

    Refactor: remove password digest from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+  acts_as_token_authenticatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
   validates_presence_of :email
   validates_uniqueness_of :email
 
-  has_secure_password
-  has_secure_token :api_key
 
   has_many :trip_users
   has_many :trips, through: :trip_users
